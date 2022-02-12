45fc41b79cfde33be2e669b1777b292a9a5d9a27
wentao.li <wentao.li@moji.com>
2022-02-12 15:16:57 +0800

    feat: load_dem增加对京津冀地区的范围选择功能

--- cnmaps/sample.py
+++ cnmaps/sample.py
@@ -7,11 +7,25 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DATA_DIR = os.path.join(BASE_DIR, 'data', 'sample')
 
 
-def load_dem():
+def load_dem(area_name=None):
+    """加载海拔高度样例数据
+
+    参数:
+        area_name (str, 可选): 区域名称, 若为None则取全国. 默认为 None.
+
+    返回值:
+        tuple: (lons, lats, data)
+    """
+    SUB_AREA = {'京津冀': np.s_[95:149, 207:259]}
     ds = nc.Dataset(os.path.join(BASE_DATA_DIR, 'china_dem.nc'))
-    data = ds.variables['dem'][:]
     lon = ds.variables['lon'][:]
     lat = ds.variables['lat'][:]
     lons, lats = np.meshgrid(lon, lat)
+    if area_name:
+        data = ds.variables['dem'][SUB_AREA[area_name]]
+        lons = lons[SUB_AREA[area_name]]
+        lats = lats[SUB_AREA[area_name]]
+    else:
+        data = ds.variables['dem'][:]
 
     return lons, lats, data
