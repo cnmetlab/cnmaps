75a361cb9505637642195fa13241a0b643f5ece9
Wentao Li <clarmylee92510@gmail.com>
2022-02-16 02:08:56 +0800

    fix: 重新整理了一遍边界数据文件， 修复了边界错误的问题。

--- tests/test_map.py
+++ tests/test_map.py
@@ -37,13 +37,14 @@ def test_map_operator():
     assert isinstance(
         get_map('四川').union(get_map('重庆')) + get_map('贵州'), MapPolygon)
     assert round((get_map('四川') + get_map('重庆') + get_map('贵州')).area,
-                 2) == 69.22
+                 2) == 69.45
 
     # 逻辑与操作符(交集)
     assert isinstance(
         get_map('青藏高原', map_set='geography') & get_map('四川'), MapPolygon)
+
     assert round((get_map('青藏高原', map_set='geography') & get_map('四川')).area,
-                 2) == 23.92
+                 2) == 24.13
     assert isinstance(get_map('福建') & get_map('广东'), MapPolygon)
 
     # & 和 + 混合
@@ -63,14 +64,14 @@ def test_map_operator():
         get_map('云南'), MapPolygon)
     assert isinstance(
         get_map('中国') - get_map('山西') + get_map('山西'), MapPolygon)
-    assert round((get_map('中国') - get_map('山西')).area, 2) == 945.33
+    assert round((get_map('中国') - get_map('山西')).area, 2) == 945.3
 
 
 def test_province_orthogonality():
     """检验省边界地图的正交性"""
     name_keys = set(NAMES['default'].keys()) - set(
-        [('南海', '九段线', 'MaritimeBoundary'), ('中国', '国界线', 'ChinaLand'),
-         ('中国2', '国界线2', 'ChinaLand2')])
+        [('南海', '九段线', 'MaritimeBoundary'), ('中国', '国界线', '简化中国', 'ChinaLand'),
+         ('中国2', '国界线2', '完整中国', 'ChinaLand2')])
     province_names = [nk[0] for nk in name_keys]
 
     couples = sorted([couple for couple in combinations(province_names, r=2)])
@@ -82,8 +83,8 @@ def test_province_orthogonality():
 def test_province_union():
     """检验所有省量量相加是否会报错"""
     name_keys = set(NAMES['default'].keys()) - set(
-        [('南海', '九段线', 'MaritimeBoundary'), ('中国', '国界线', 'ChinaLand'),
-         ('中国2', '国界线2', 'ChinaLand2')])
+        [('南海', '九段线', 'MaritimeBoundary'), ('中国', '国界线', '简化中国', 'ChinaLand'),
+         ('中国2', '国界线2', '完整中国', 'ChinaLand2')])
     province_names = [nk[0] for nk in name_keys]
 
     couples = sorted([couple for couple in combinations(province_names, r=2)])
@@ -95,8 +96,8 @@ def test_province_union():
 def test_province_difference():
     """检验所有省量量相减是否会报错"""
     name_keys = set(NAMES['default'].keys()) - set(
-        [('南海', '九段线', 'MaritimeBoundary'), ('中国', '国界线', 'ChinaLand'),
-         ('中国2', '国界线2', 'ChinaLand2')])
+        [('南海', '九段线', 'MaritimeBoundary'), ('中国', '国界线', '简化中国', 'ChinaLand'),
+         ('中国2', '国界线2', '完整中国', 'ChinaLand2')])
     province_names = [nk[0] for nk in name_keys]
 
     couples = sorted([couple for couple in product(province_names, repeat=2)])
@@ -106,14 +107,18 @@ def test_province_difference():
 
 
 def test_get_extent():
-    extent = [(109.661377, 117.304153, 20.217283, 25.518644332885742),
-              (108.66138099701756, 118.3032636214493, 19.21844606457509,
-               26.51787665743222),
-              (107.66138499403512, 119.30237424289862, 18.219609129150186,
-               27.51742946075122),
-              (106.66138899105268, 120.30148486434793, 17.220772193725278,
-               28.516822024683957),
-              (105.66139298807025, 121.30059548579725, 16.22193525830037,
-               29.51502654056799)]
+    extent = [(109.650798, 117.313723, 20.214186, 25.519625),
+              (108.65117139025172, 118.31362977796758, 19.214262701394475,
+               26.51899149752301),
+              (107.65154478050344, 119.31353655593516, 18.214339402788948,
+               27.51850580083624),
+              (106.65191817075515, 120.31344333390275, 17.21441610418342,
+               28.51849570125436),
+              (105.65229156100688, 121.31335011187034, 16.21449280557789,
+               29.518790750222323)]
     for i in range(5):
         assert get_map('广东').get_extent(buffer=i) == extent[i]
+
+
+if __name__ == '__main__':
+    test_map_operator()
