f672ecdb27d15a4cbb46bd975124ea63931896fe
Clarmy Lee <clarmylee92510@gmail.com>
2022-07-12 10:42:04 +0800

    refactor: use single version and fix dependency.

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 import setuptools
 import os
+import codecs
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r", encoding="utf-8") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -15,7 +32,7 @@ with open(requirements_path, "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="cnmaps",
-    version="1.0.1",
+    version=get_version("cyeva/__init__.py"),
     author="Wentao Li",
     author_email="clarmylee92510@gmail.com",
     description="A python package to draw china maps more easily",
