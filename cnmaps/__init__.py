520eecd031e76d36b6d7f95312126556a04ca28c
Clarmy Lee <clarmylee92510@gmail.com>
2022-08-05 11:53:39 +0800

    style: reformat by black

--- cnmaps/__init__.py
+++ cnmaps/__init__.py
@@ -13,11 +13,7 @@ __version__ = "1.1.0"
 
 CARTOPY_DIGIT_VERSION = re.match(r"(\d*\.\d*\.\d*)", cartopy.__version__).group(1)
 if CARTOPY_DIGIT_VERSION < "0.20.0":
-    warnings.warn(
-        (
-            "由于Cartopy的版本低于0.20.0, 请将Cartopy的版本升级到0.20.0及以上."
-        )
-    )
+    warnings.warn(("由于Cartopy的版本低于0.20.0, 请将Cartopy的版本升级到0.20.0及以上."))
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DATA_DIR = os.path.join(BASE_DIR, "data", "geojson.min")
