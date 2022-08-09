135cb588b6fd86d8237712cd6507dc3e851eb1bf
Clarmy Lee <clarmylee92510@gmail.com>
2022-08-10 01:28:43 +0800

    feat: add load_temp and load_wind

--- cnmaps/sample.py
+++ cnmaps/sample.py
@@ -32,3 +32,36 @@ def load_dem(area_name=None):
         data = ds.variables["dem"][:]
 
     return lons, lats, data
+
+
+def load_wind():
+    """
+    加载风矢样例数据
+
+    返回值:
+        tuple: (lons, lats, u, v)
+    """
+    ds = nc.Dataset(os.path.join(BASE_DATA_DIR, "china-wind.nc"))
+    lon = ds.variables["lon"][:]
+    lat = ds.variables["lat"][:]
+    lons, lats = np.meshgrid(lon, lat)
+    u = ds.variables["u"][:]
+    v = ds.variables["v"][:]
+
+    return lons, lats, u, v
+
+
+def load_temp():
+    """
+    加载气温样例数据
+
+    返回值:
+        tuple: (lons, lats, temp)
+    """
+    ds = nc.Dataset(os.path.join(BASE_DATA_DIR, "china-temp.nc"))
+    lon = ds.variables["lon"][:]
+    lat = ds.variables["lat"][:]
+    lons, lats = np.meshgrid(lon, lat)
+    temp = ds.variables["temp"][:]
+
+    return lons, lats, temp
