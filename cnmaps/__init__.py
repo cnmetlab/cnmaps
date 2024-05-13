724dd4031a765af8e86ea06941245e1612147ef3
Clarmy Lee <clarmylee92510@gmail.com>
2024-05-13 16:58:30 +0800

    chore[skip ci]: add version warning for shapely.

--- cnmaps/__init__.py
+++ cnmaps/__init__.py
@@ -5,7 +5,9 @@ import re
 import warnings
 
 import cartopy
+import shapely
 from shapely.errors import ShapelyDeprecationWarning
+
 
 from .maps import *  # noqa: F403, F401
 from .drawing import *  # noqa: F403, F401
@@ -14,7 +16,11 @@ __version__ = "1.1.8"
 
 CARTOPY_DIGIT_VERSION = re.match(r"(\d*\.\d*\.\d*)", cartopy.__version__).group(1)
 if CARTOPY_DIGIT_VERSION < "0.22.0":
-    warnings.warn(("由于Cartopy的版本低于0.22.0, 请将Cartopy的版本升级到0.22.0及以上."))
+    warnings.warn(("由于Cartopy的版本低于0.22.0, 请将Cartopy的版本升级到0.22.0及以上, 否则会影响功能的使用"))
+
+SHAPELY_DIGIT_VERSION = re.match(r"(\d*\.\d*\.\d*)", shapely.__version__).group(1)
+if SHAPELY_DIGIT_VERSION >= "2.0.0":
+    warnings.warn(("由于Shapely的版本高于2.0.0, 请将Shapely的版本降级到2.0.0以下, 否则会影响功能的使用"))
 
 warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
 
